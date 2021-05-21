0e99d8025e0dfd1d2218e4f181e7e535b39d63e7
Angelo Faella <revangel@outlook.it>
2021-05-21 16:27:44 +0200

    Add no-results label

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import '../styles/App.css';
 import Quote from './Quote';
@@ -7,14 +7,16 @@ import { requestQuotes } from '../apis/animeChan';
 
 const App = () => {
   const [quotes, setQuotes] = useState([]);
+  const [noResults, setNoResults] = useState(false);
 
-  const onSearchSubmit = async term => {
+  const onSearchSubmit = useCallback(async term => {
     console.log('New Search submit:', term);
     const quotesArray = await requestQuotes(term);
+    setNoResults(quotesArray.length === 0);
     setQuotes(quotesArray);
-  };
+  });
 
-  const clearResults = () => setQuotes([]);
+  const clearResults = useCallback(() => setQuotes([]));
 
   const renderedQuotes = quotes.map((quote, i) => {
     return <Quote quote={quote} key={i} />
@@ -22,18 +24,21 @@ const App = () => {
 
   return (
     <div className='app'>
-      <h1 className='title'>Realtime Search Bar</h1>
+      <h1 className='title'>Search Quotes</h1>
 
+      <div className='disclaimer-container'>
+        <p className='disclaimer'>
+          Get 10 quotes from your favorite <span className='highlight'>anime</span>!
+        </p>
+      </div>
+      
       <SearchBar onSearchSubmit={onSearchSubmit} clearResults={clearResults}/>
 
-      {/* { renderedQuotes.length === 0 &&
-        <div className='disclaimer-container'>
-          <p className='disclaimer'>
-            <a href="https://github.com/AngeloFaella">Angelo Faella</a>
-          </p>
-        </div>
-      } */}
-
+      { noResults &&
+        <p className='no-results'>
+          No results found.
+        </p>
+      }
       <div className='main-content'>
         {renderedQuotes}
       </div>
